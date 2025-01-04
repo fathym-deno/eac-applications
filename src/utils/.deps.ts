41fe843189abd7c07e5b8dc5a041e70f48024301
mcgear <michael.gearhardt@gmail.com>
2025-01-03 22:18:42 -0700

    resolve dynamic values

--- src/utils/.deps.ts
+++ src/utils/.deps.ts
@@ -4,3 +4,5 @@ export type {
   EverythingAsCode,
 } from "jsr:@fathym/eac@0.2.28";
 export { callEaCActuatorConnections } from "jsr:@fathym/eac@0.2.28/steward/utils";
+
+export * as jsonpath from "npm:jsonpath@1.1.1";
--- src/utils/.exports.ts
+++ src/utils/.exports.ts
@@ -1,1 +1,2 @@
 export * from "./loadConnections.ts";
+export * from "./resolveDynamicValues.ts";
