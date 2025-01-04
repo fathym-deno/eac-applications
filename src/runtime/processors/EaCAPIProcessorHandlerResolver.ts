7b5b0803a22c8ef32c61f42d518c8da5b76e8d93
mcgear <michael.gearhardt@gmail.com>
2025-01-03 18:10:12 -0700

    better logging

--- src/runtime/processors/EaCAPIProcessorHandlerResolver.ts
+++ src/runtime/processors/EaCAPIProcessorHandlerResolver.ts
@@ -24,75 +24,90 @@ export const EaCAPIProcessorHandlerResolver: ProcessorHandlerResolver = {
 
     const processor = appProcCfg.Application.Processor as EaCAPIProcessor;
 
-    const dfs = eac.DFSs![processor.DFSLookup]!.Details!;
+    try {
+      const dfs = eac.DFSs![processor.DFSLookup]!.Details!;
 
-    const fileHandler = await loadFileHandler(ioc, dfs);
+      const fileHandler = await loadFileHandler(ioc, dfs);
 
-    const patterns = await loadRequestPathPatterns<{
-      middleware: [string, EaCRuntimeHandlerSet][];
-    }>(
-      fileHandler!,
-      dfs,
-      async (allPaths) => {
-        const middlewareLoader = async () => {
-          const middlewarePaths = allPaths
-            .filter((p) => p.endsWith("_middleware.ts"))
-            .sort((a, b) => a.split("/").length - b.split("/").length);
+      const patterns = await loadRequestPathPatterns<{
+        middleware: [string, EaCRuntimeHandlerSet][];
+      }>(
+        fileHandler!,
+        dfs,
+        async (allPaths) => {
+          const middlewareLoader = async () => {
+            const middlewarePaths = allPaths
+              .filter((p) => p.endsWith("_middleware.ts"))
+              .sort((a, b) => a.split("/").length - b.split("/").length);
 
-          const middlewareCalls = middlewarePaths.map((p) => {
-            return loadMiddleware(
-              logger,
-              fileHandler!,
-              p,
-              dfs,
-              processor.DFSLookup,
+            const middlewareCalls = middlewarePaths.map((p) => {
+              return loadMiddleware(
+                logger,
+                fileHandler!,
+                p,
+                dfs,
+                processor.DFSLookup,
+              );
+            });
+
+            return (await Promise.all(middlewareCalls))
+              .filter((m) => m)
+              .map((m) => m!);
+          };
+
+          const [middleware] = await Promise.all([middlewareLoader()]);
+
+          logger.debug(`Middleware ${processor.DFSLookup}: `);
+          logger.debug(middleware.map((m) => m));
+          logger.debug("");
+
+          return { middleware };
+        },
+        async (filePath) => {
+          return await loadEaCRuntimeHandlers(
+            logger,
+            fileHandler!,
+            filePath,
+            dfs,
+            processor.DFSLookup,
+          );
+        },
+        (filePath, pipeline, { middleware }) => {
+          const reqMiddleware = middleware
+            .filter(([root]) => {
+              return filePath.startsWith(root);
+            })
+            .flatMap(([_root, handler]) =>
+              Array.isArray(handler) ? handler : [handler]
             );
-          });
 
-          return (await Promise.all(middlewareCalls))
-            .filter((m) => m)
-            .map((m) => m!);
-        };
-
-        const [middleware] = await Promise.all([middlewareLoader()]);
-
-        logger.debug(`Middleware ${processor.DFSLookup}: `);
-        logger.debug(middleware.map((m) => m));
+          pipeline.Prepend(...reqMiddleware);
+        },
+        appProcCfg.Revision,
+      ).then((patterns) => {
+        logger.debug(`APIs ${processor.DFSLookup}: `);
+        logger.debug(patterns.map((p) => p.PatternText));
         logger.debug("");
 
-        return { middleware };
-      },
-      async (filePath) => {
-        return await loadEaCRuntimeHandlers(
-          logger,
-          fileHandler!,
-          filePath,
-          dfs,
-          processor.DFSLookup,
+        return patterns;
+      });
+
+      return (req, ctx) => {
+        return executePathMatch(
+          patterns,
+          req,
+          ctx,
+          processor.DefaultContentType,
         );
-      },
-      (filePath, pipeline, { middleware }) => {
-        const reqMiddleware = middleware
-          .filter(([root]) => {
-            return filePath.startsWith(root);
-          })
-          .flatMap(([_root, handler]) =>
-            Array.isArray(handler) ? handler : [handler]
-          );
+      };
+    } catch (err) {
+      if (err instanceof Error) {
+        logger.error(
+          `Error processing ${appProcCfg.ApplicationLookup} API processor: ${err.message}`,
+        );
+      }
 
-        pipeline.Prepend(...reqMiddleware);
-      },
-      appProcCfg.Revision,
-    ).then((patterns) => {
-      logger.debug(`APIs ${processor.DFSLookup}: `);
-      logger.debug(patterns.map((p) => p.PatternText));
-      logger.debug("");
-
-      return patterns;
-    });
-
-    return (req, ctx) => {
-      return executePathMatch(patterns, req, ctx, processor.DefaultContentType);
-    };
+      throw err;
+    }
   },
 };
