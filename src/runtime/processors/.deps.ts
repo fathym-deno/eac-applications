81dfcc4b5c3d0686b354a39ec2f8f1bb1236ec76
mcgear <michael.gearhardt@gmail.com>
2025-09-09 12:14:47 -0600

    ref updates

--- src/runtime/processors/.deps.ts
+++ src/runtime/processors/.deps.ts
@@ -41,7 +41,7 @@ export {
   isEaCAzureADProviderDetails,
   isEaCGitHubAppProviderDetails,
   isEaCOAuthProviderDetails,
-} from "jsr:@fathym/eac-identity@0.0.23";
+} from "jsr:@fathym/eac-identity@0.0.24";
 
 export { IoCContainer } from "jsr:@fathym/ioc@0.0.14";
 
